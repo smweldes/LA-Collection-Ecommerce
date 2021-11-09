1fdbe5f1e4a163f53aa4fbbecd4cc4525ed4e317
smweldes <weldesiem@gmail.com>
2021-11-08 23:23:27 -0800

    home page component

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './main.css'
+
+function Home() {
+    return (
+        <div className="hero">
+            <div class="card bg-dark text-white">
+               <img src="https://hips.hearstapps.com/mac.h-cdn.co/assets/15/30/2560x1280/landscape-1437670852-nafisa-rahman.jpg?resize=1200:*" className="card-img" alt="Background"
+               height="500px"/>
+        <div class="card-img-overlay d-flex-column justify-content-center">
+           <h5 class="card-title display-3 fw-bolder mb-0 ">NEW SEASON ARRIVALS</h5>
+           <p class="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+           
+       </div>
+</div>
+        </div>
+    )
+}
+
+export default Home
